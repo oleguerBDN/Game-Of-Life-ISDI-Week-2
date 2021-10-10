167ead3aff0b87004c8d4483596436736ac8c98b
Oleguer Carreras <oleguer.6.bdn@hotmail.com>
2021-10-10 12:00:47 +0200

    Added pause-resume with space bar key

--- src/gameOfLife.js
+++ src/gameOfLife.js
@@ -2,29 +2,31 @@
 
 import { GameBoard } from "./gameClass.js";
 
-const k = new GameBoard(10);
+const myGame = new GameBoard(10);
 
-k.board[8][8] = 1;
-k.board[1][0] = 1;
-k.board[1][1] = 1;
-k.board[1][2] = 1;
-k.board[5][2] = 1;
-k.board[5][3] = 1;
-k.board[5][4] = 1;
-k.board[7][4] = 1;
-k.board[8][9] = 1;
+myGame.printInitBoard();
 
-k.printInitBoard();
+let interval = null;
 
-let p = setInterval(function () {
-  k.board = k.updatedCopy();
-  k.updatePrintedBoard();
-}, 3000);
+// Space button to pause and replay
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Space") {
+    if (interval === null) {
+      interval = setInterval(function () {
+        myGame.board = myGame.updatedCopy();
+        myGame.updatePrintedBoard();
+      }, 2000);
+    } else {
+      clearInterval(interval);
+      interval = null;
+    }
+  }
+});
 
 // All the functionality when element is clicked goes here :)
 let onClick = (clickedItem) => {
   if ((clickedItem.target.parentNode.id = "row")) {
-    k.clickedCell(clickedItem.target);
+    myGame.clickedCell(clickedItem.target);
   }
 };
 
@@ -35,12 +37,5 @@ let assignEvents = (element) => {
 
 // Assign events to all the cells
 document
-  .querySelectorAll("." + k.deathClass + " , ." + k.aliveClass)
+  .querySelectorAll("." + myGame.deathClass + " , ." + myGame.aliveClass)
   .forEach((cell) => assignEvents(cell));
-
-//const matches = document.querySelectorAll(".death , .alive");
-
-//const elementsToListen = [];
-//matches.forEach((cell) => elementsToListen.push(cell));
-
-//elementsToListen.forEach(assignEvents);
